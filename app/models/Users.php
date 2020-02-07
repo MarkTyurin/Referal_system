3371b87486038abcb53b36c2ef4304bbff52089c
TyurinMark <tyurin.mark@bk.ru>
2020-02-07 17:23:26 +0300

    jsontest3

--- app/models/Users.php
+++ app/models/Users.php
@@ -6,8 +6,8 @@ class Users {
     public function getListUsers(){
         #$result = _MainModel::table("users_cards")->get()->send();
         $aaa = array("id"=>2,"name"=>"aaa");
-		$json = json_encode($aaa);
-        _MainModel::viewJSON($json);   
+		#$json = json_encode($aaa);
+        _MainModel::viewJSON($aaa);   
     }
 
 }
